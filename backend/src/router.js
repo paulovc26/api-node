76175fbc41a662c2f48195425883360a8d1b5529
paulovc26 <102874351+paulovc26@users.noreply.github.com>
2024-04-23 13:56:08 -0300

    update router - new middleware

--- backend/src/router.js
+++ backend/src/router.js
@@ -6,9 +6,9 @@ const tasksController = require("./controllers/tasksController");
 const taskMiddleware = require("./middlewares/tasksMiddleware");
 
 router.get('/tasks', tasksController.getAll);
-router.post('/tasks', taskMiddleware.validateBody, tasksController.createTask);
+router.post('/tasks', taskMiddleware.validateTitle, tasksController.createTask);
 router.delete('/tasks/:id', tasksController.deleteTask);
-router.put('/tasks/:id', taskMiddleware.validateBody, tasksController.updateTask);
+router.put('/tasks/:id', taskMiddleware.validateTitle, taskMiddleware.validateStatus, tasksController.updateTask);
 
 
 module.exports = router;
